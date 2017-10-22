a3a4d34ccb1331972b74300c66a587e1c967f080
rupal19 <rupalsharma19293@gmail.com>
2017-10-22 19:28:31 +0530

    Submitting assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions check if the inverse of the matrix already exists returns its value
+## If the inverse doesno exists it calculates it's value
 
-## Write a short comment describing this function
+## Returns a list which contains the set, get, setinverse, getinverse function
+## Also contains the cached inverse of the matrix if has been previously calculated
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+        set <- function(y) {
+                mat <<- y
+                inv <<- NULL
+        }
+        get <- function() mat
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function calculates the inverse of the matrix if it exists
+## The arguments to this function is the list returned from the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	    m <- x$getinverse()
+        if ( !is.null (m) ) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+	if( det (data) != 0 ) {
+        m <- solve(data)
+        x$setinverse(m)
+        m  ## Return a matrix that is the inverse of 'x'
+	}
+	else 
+	message("Inverse cannot be calculated as determinant is 0") ## Return an error messgae if the matrix is not invertible
+       
 }
